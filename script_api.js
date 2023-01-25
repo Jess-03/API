5b146f7f932d22993761b22b871a2976c5a0f1b2
Jessica Garcia <jessicavgco@gmail.com>
2023-01-24 20:25:50 -0600

    fetch funciona

---
+++ script_api.js
@@ -0,0 +1,60 @@
+const showData = (users) => {
+    let showTableData = ""
+    for (let i = 0; i < users.length; i++) {
+        showTableData += `<tr>
+                        <td>${users[i].id}</td>
+                        <td>${users[i].first_name}</td>
+                        <td>${users[i].last_name}</td>
+                        <td>${users[i].email}</td>
+                        <td>
+                            <img src="${users[i].avatar}">
+                        </td>
+                    </tr>`
+    };
+    document.getElementById('data').innerHTML = showTableData;
+}
+
+let tiempoDeVida;
+
+const btnGetUsers = document.getElementById("getUsers")
+btnGetUsers.addEventListener('click', () => {
+
+    const url = 'https://reqres.in/api/users?delay=3';
+    if ((typeof (tiempoDeVida) == "undefined") ||
+        new Date().getTime() - tiempoDeVida > 60_000) {
+
+        fetch(url)
+            .then(response => response.json())
+            .then(users => {
+                console.log(users)
+                localStorage.setItem("datosguardadosUsuarios", JSON.stringify(users.data));
+                localStorage.setItem("tiempoDeVida", (new Date().getTime()) + 60_000);
+                tiempoDeVida = localStorage.getItem('tiempoDeVida');
+                showData(users.data);
+            })
+
+            .catch(error => console.log(error));
+    }
+    else {
+        showData(JSON.parse(localStorage.getItem("datosguardadosUsuarios")));
+        console.log("Datos locales");
+    }
+        
+});
+
+const datosConvertidos = JSON.parse(localStorage.getItem("datosguardadosUsuarios"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
